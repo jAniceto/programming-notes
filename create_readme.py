813f5797d36c4977f9c4711152571031a6a6637b
José Aniceto <jAniceto@users.noreply.github.com>
2022-03-18 11:22:50 +0000

    Update create_readme.py

--- create_readme.py
+++ create_readme.py
@@ -1,22 +1,34 @@
-import os
+# import os
+from pathlib import Path
+
 
 # Configuration
 TITLE = 'Programming Notes'
-
-SUBTITLE = 'Programming notes, snippets and examples in one place.'
-
+SUBTITLE = 'Programming notes, snippets and examples.'
 DISCLAIMER = 'These notes contain excerpts, code snippets and examples from various sources including, but not limited to, Python Docs, The Hitchhiker’s Guide to Python, Real Python and Stack Overflow users.'
-
 EXCLUDE_DIRS = ['.git', '.github', '.idea', '__pycache__', 'articles', 'docs']
 
 
-def get_md_files(directory):
-    """Get all Markdown files in directory"""
-    md_files = []
-    for file in os.listdir(directory):
-        if file.endswith(".md"):
-            md_files.append(os.path.join(directory, file))
-    return md_files
+def get_directories():
+    """Get directories in current folder. 
+
+    RETURNS
+        list of pathlib.Path
+    """
+    cwd = Path.cwd()
+    return [p for p in cwd.iterdir() if p.is_dir() and p.name not in EXCLUDE_DIRS]
+
+
+def get_md_files(path):
+    """Get all Markdown files in directory and sort. 
+
+    ARGUMENTS
+        path : pathlib.Path
+
+    RETURNS
+        list of pathlib.Path
+    """
+    return [p for p in path.iterdir() if p.is_file() and p.suffix == '.md']
 
 
 def get_article_title(filename):
@@ -27,20 +39,18 @@ def get_article_title(filename):
 
 
 def main():
-    # Filter the result using os.path.isdir() (and use os.path.join() to get the real path):
-    sections = [ name for name in os.listdir('.') if os.path.isdir(os.path.join('.', name)) and name not in EXCLUDE_DIRS ]
+    sections = get_directories()
 
     index_str = ''
     for section in sections:
-        index_str += '## ' + section.replace('-', ' ').upper() + '\n'
+        index_str += '## ' + section.stem.replace('-', ' ').upper() + '\n'
 
         md_files = get_md_files(section)
 
         for md_file in md_files:
             title = get_article_title(md_file)
-            index_str += f'* [{title}](/{md_file})\n'.replace('\\', '/')
+            index_str += f"- [{title}](/{md_file.parent.name}/{md_file.name})\n"
         index_str += '\n'
-
 
     # Build Readme.md
     readme = f"""# {TITLE}
